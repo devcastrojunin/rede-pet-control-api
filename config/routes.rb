87bbfd0c7184be0efa64f02cfd7bc9374cb9533e
devcastrojunin <devcastrojunin@gmail.com>
2022-06-10 11:54:04 -0300

    New Routes API

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
-      resources :pets
       resources :addresses
       resources :clientes
+      resources :doctors
+      resources :pets
       resources :roles
+      resources :schedules
+      resources :users
       post "/login", to: "users#login"
       get "/auto_login", to: "users#auto_login"
     end
