8f6f7e7bd2a8347d6c50b87fa8c85f8bd1f602b0
devcastrojunin <devcastrojunin@gmail.com>
2022-06-13 18:26:08 -0300

    Unit test User

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { "Junior Castro" }
+    username { "junior.castro" }
+    password { "teste@123" }
+    role
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,17 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe "validations" do
+  describe "Fields validation" do
+    it "User was created" do
+      user = create(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  describe "Elements presence" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:role_id) }
   end
 end
