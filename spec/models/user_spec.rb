a5cc022c0ab642653cca8b58de5a97aaf86762c5
devcastrojunin <devcastrojunin@gmail.com>
2022-06-14 15:02:16 -0300

    New scenarios to test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,28 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe "Fields validation" do
-    it "User was created" do
-      user = create(:user)
-      expect(user).to be_valid
-    end
-  end
+  let!(:user) { create(:user) }
 
-  describe "Elements presence" do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:username) }
-    it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:role_id) }
+  context "User validations" do
+    describe "Fields validation" do
+      it "User should be created" do
+        expect(user).to be_valid
+      end
+
+      it "Username should be unique" do
+        should validate_uniqueness_of(:username)
+      end
+
+      it "User should be one role" do
+        should belong_to(:role)
+      end
+    end
+
+    describe "Fields required" do
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:username) }
+      it { should validate_presence_of(:password) }
+      it { should validate_presence_of(:role_id) }
+    end
   end
 end
